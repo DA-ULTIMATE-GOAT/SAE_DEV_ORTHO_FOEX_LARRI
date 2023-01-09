6e574155fd4c62b624e43367ab01abb0ed2a1990
DA-BINOR <robin.foex@etu.univ-smb.fr>
2023-01-09 10:23:59 +0100

    attaque perso dans salle principale

--- CHADventure/CHADventure/SallePrincipale.cs
+++ CHADventure/CHADventure/SallePrincipale.cs
@@ -48,8 +48,9 @@ namespace CHADventure
         public override void Update(GameTime gameTime)
         {
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            _perso.DeplacementPerso(gameTime, _tiledMap, _mapLayer, _mapLayer2);
+            _perso.Attaque(gameTime);
+            if (!_perso._attaque)
+                _perso.DeplacementPerso(gameTime, _tiledMap, _mapLayer, _mapLayer2);
             _tiledMapRenderer.Update(gameTime);
             _perso._ezioSprite.Play(_perso._animation);
             _perso._ezioSprite.Update(deltaTime);
