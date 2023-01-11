5502a16019733a54382dd694c52ac0f0f09becee
DA-BINOR <robin.foex@etu.univ-smb.fr>
2023-01-11 15:53:34 +0100

    faut faire les dégats

--- CHADventure/CHADventure/RedBlob.cs
+++ CHADventure/CHADventure/RedBlob.cs
@@ -33,10 +33,11 @@ namespace CHADventure
         }
 
         public Perso Perso { get => _perso; set => _perso = value; }
+        public Vector2 PositionBlob { get => _positionBlob; set => _positionBlob = value; }
 
         public void Initialize()
         {
-            _positionBlob = new Vector2(rndm.Next(288, 496), rndm.Next(256, 464));
+            PositionBlob = new Vector2(rndm.Next(288, 496), rndm.Next(256, 464));
             _vitesse = rndm.Next(VITESSE_MIN_BLOB, VITESSE_MAX_BLOB);
         }
         public void LoadContent(Game1 game)
@@ -50,47 +51,47 @@ namespace CHADventure
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
             _spriteBlob.Play(_animationBlob);
             _spriteBlob.Update(gameTime);
-            if (_positionBlob.X > Perso._positionPerso.X)
+            if (PositionBlob.X > Perso._positionPerso.X)
             {
-                ushort tx = (ushort)(_positionBlob.X / _tiledMap.TileWidth + 1);
-                ushort ty = (ushort)(_positionBlob.Y / _tiledMap.TileHeight + 1);
+                ushort tx = (ushort)(PositionBlob.X / _tiledMap.TileWidth + 1);
+                ushort ty = (ushort)(PositionBlob.Y / _tiledMap.TileHeight + 1);
                 _animationBlob = "walkEast";
 
                 if (!IsCollision(tx, ty, _mapLayer, _mapLayer2))
                     _positionBlob.X -= _vitesse * deltaTime;
             }
-            if (_positionBlob.X < Perso._positionPerso.X)
+            if (PositionBlob.X < Perso._positionPerso.X)
             {
-                ushort tx = (ushort)(_positionBlob.X / _tiledMap.TileWidth - 1);
-                ushort ty = (ushort)(_positionBlob.Y / _tiledMap.TileHeight + 1);
+                ushort tx = (ushort)(PositionBlob.X / _tiledMap.TileWidth - 1);
+                ushort ty = (ushort)(PositionBlob.Y / _tiledMap.TileHeight + 1);
                 _animationBlob = "walkWest";
 
                 if (!IsCollision(tx, ty, _mapLayer, _mapLayer2))
                     _positionBlob.X += _vitesse * deltaTime;
             }
-            if (_positionBlob.Y > Perso._positionPerso.Y)
+            if (PositionBlob.Y > Perso._positionPerso.Y)
             {
-                ushort tx = (ushort)(_positionBlob.X / _tiledMap.TileWidth);
-                ushort ty = (ushort)(_positionBlob.Y / _tiledMap.TileHeight);
+                ushort tx = (ushort)(PositionBlob.X / _tiledMap.TileWidth);
+                ushort ty = (ushort)(PositionBlob.Y / _tiledMap.TileHeight);
                 _animationBlob = "walkNorth";
 
                 if (!IsCollision(tx, ty, _mapLayer, _mapLayer2))
                     _positionBlob.Y -= _vitesse * deltaTime;
             }
-            if (_positionBlob.Y < Perso._positionPerso.Y)
+            if (PositionBlob.Y < Perso._positionPerso.Y)
             {
-                ushort tx = (ushort)(_positionBlob.X / _tiledMap.TileWidth);
-                ushort ty = (ushort)(_positionBlob.Y / _tiledMap.TileHeight + 2);
+                ushort tx = (ushort)(PositionBlob.X / _tiledMap.TileWidth);
+                ushort ty = (ushort)(PositionBlob.Y / _tiledMap.TileHeight + 2);
                 _animationBlob = "walkSouth";
 
                 if (!IsCollision(tx, ty, _mapLayer, _mapLayer2))
                     _positionBlob.Y += _vitesse * deltaTime;
             }
-            APorter(_positionBlob);
+            APorter(PositionBlob);
         }
         public void Draw(SpriteBatch spritebatch)
         {
-            spritebatch.Draw(this._spriteBlob, this._positionBlob);
+            spritebatch.Draw(this._spriteBlob, this.PositionBlob);
         }
         private bool IsCollision(ushort x, ushort y, TiledMapTileLayer _mapLayer, TiledMapTileLayer _mapLayer2)
         {
@@ -106,8 +107,8 @@ namespace CHADventure
         private bool APorter(Vector2 position)
         {
             bool touche = false;
-            position.X = Math.Abs(Perso._positionPerso.X - _positionBlob.X);
-            position.Y = Math.Abs(Perso._positionPerso.Y - _positionBlob.Y);
+            position.X = Math.Abs(Perso._positionPerso.X - PositionBlob.X);
+            position.Y = Math.Abs(Perso._positionPerso.Y - PositionBlob.Y);
 
             if(position.X <= 2 && position.Y <= 2)
             {
